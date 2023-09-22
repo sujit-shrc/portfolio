1bb2cc70ca7763bf1a8187267a502d428554e61a
Sujit <mnamesujit@gmail.com>
2023-09-23 03:39:23 +0530

    removed custom vars and home page url config

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -27,7 +27,7 @@ const Header: React.FC = () => {
       className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex"
     >
       <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 lg:static lg:w-auto lg:rounded-full lg:border lg:p-3.5 text-dark dark:text-light">
-        <code className="font-mono font-bold">FullStack/StackShow/Nucleus</code>
+        <code className="font-mono font-bold">FullStack/StackShow/Nucleus.dev</code>
       </p>
       <div
         className="fixed bottom-0 left-0 flex space-x-3 h-48 w-full items-end justify-center lg:static lg:h-auto lg:w-auto text-dark dark:text-light"
