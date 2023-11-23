67e69d4dd204527e3895c0f93d118670bc1e7e4f
Sujit <mnamesujit@gmail.com>
2023-11-24 00:14:49 +0530

    add social links in mobile layout

--- components/Header/SocialLinks/index.tsx
+++ components/Header/SocialLinks/index.tsx
@@ -6,7 +6,7 @@ interface SocialLinkProps {
   const SocialLink: React.FC<SocialLinkProps> = ({ href, icon }) => {
     return (
       <a
-        className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+        className="pointer-events-none flex items-center gap-2 px-3 py-2 md:p-8 lg:pointer-events-auto lg:p-0"
         href={href}
         target="_blank"
         rel="noopener noreferrer"
--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,28 +1,39 @@
+"use client"
+
+import { useState } from "react";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaFilePdf } from "react-icons/fa6";
 import { ImGithub } from "react-icons/im";
 import { GrTwitter } from "react-icons/gr";
 import { PiInstagramLogoFill } from "react-icons/pi";
+import { TbSocial } from "react-icons/tb";
 import SocialLink from "./SocialLinks";
 import ThemeSwitch from "../ThemeSwitcher";
 import ScanMe from "../ScanMe";
 import Link from "next/link";
 
 const socialLinks = [
-  { href: "https://github.com/mnamesujit", icon: <ImGithub size={30} className="" /> },
+  { href: "https://github.com/mnamesujit", icon: <ImGithub className="text-3xl" /> },
   {
     href: "https://www.linkedin.com/in/mnamesujit",
-    icon: <FaLinkedinIn size={30} className="" />,
+    icon: <FaLinkedinIn className="text-3xl" />,
   },
   {
     href: "#",
-    icon: <PiInstagramLogoFill size={30} className="" />,
+    icon: <PiInstagramLogoFill className="text-3xl" />,
   },
-  { href: "https://twitter.com/mnamesujit", icon: <GrTwitter size={30} className="" /> },
-  { href: "/sujit-kumar.pdf", icon: <FaFilePdf size={28} className="" download = "sujit-kumar.pdf" /> },
+  { href: "https://twitter.com/mnamesujit", icon: <GrTwitter className="text-3xl" /> },
+  { href: "/sujit-kumar.pdf", icon: <FaFilePdf className="text-[1.7rem]" download = "sujit-kumar.pdf" /> },
 ];
 
 const Header: React.FC = () => {
+
+  const [showSocialLinks, setShowSocialLinks] = useState(false);
+
+  const toggleSocialLinks = () => {
+    setShowSocialLinks(!showSocialLinks);
+  };
+
   return (
     <div className="z-10 max-w-5xl w-full flex items-center justify-between font-mono px-0 pr-4 md:px-6 border-b border-gray-300 dark:border-neutral-800 md:border-none">
       <Link href={"/"}>
@@ -33,16 +44,27 @@ const Header: React.FC = () => {
           </code>
         </p>
       </Link>
-      <div className="hidden lg:flex space-x-3 items-end justify-center lg:h-auto lg:w-auto">
-        {socialLinks.map((link, index) => (
-          <SocialLink key={index} href={link.href} icon={link.icon} />
-        ))}
+
+      <div className="hidden lg:flex space-x-3 items-end justify-center lg:h-auto lg:w-auto group-hover:flex">
+            {socialLinks.map((link, index) => (
+              <SocialLink key={index} href={link.href} icon={link.icon} />
+            ))}
       </div>
+
+      {/* Mobile View for Social links */}
+      <TbSocial size={50} className="sm:hidden fixed left-[13px] bottom-0 z-50 bg-white text-black rounded-full cursor-pointer p-3 mb-3 animate-bounce ${showSocialLinks ? 'animate-none' : ''}`}" onClick = {toggleSocialLinks} />
+       {showSocialLinks && (
+        <div className="sm:hidden fixed left-3 bottom-9 py-2 pb-12 bg-neutral-800 rounded-full z-40 text-md">
+          {socialLinks.map((link, index) => (
+            <SocialLink key={index} href={link.href} icon={link.icon} />
+          ))}
+        </div>
+      )}
 
       <div className="flex items-center gap-4 md:gap-8">
         {/* Theme Switcher */}
         <ThemeSwitch />
-        <ScanMe />
+        <ScanMe />  
       </div>
     </div>
   );
