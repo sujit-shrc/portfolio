33ba322d79b47799c18b103789306da77be1896b
Sujit <mnamesujit@gmail.com>
2023-12-23 11:33:02 +0000

    feat/navbar-update

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,9 +5,9 @@ import { FaLinkedinIn } from "react-icons/fa";
 import { FaFilePdf } from "react-icons/fa6";
 import { ImGithub } from "react-icons/im";
 import { IoExit } from "react-icons/io5";
-import { GrTwitter } from "react-icons/gr";
-import { PiInstagramLogoFill } from "react-icons/pi";
-import { RiWhatsappFill } from "react-icons/ri";
+import { FaGithubAlt } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+import { VscTerminalBash } from "react-icons/vsc";
 import { TbSocial } from "react-icons/tb";
 import SocialLink from "./SocialLinks";
 import ThemeSwitch from "../ThemeSwitcher";
@@ -15,21 +15,14 @@ import ScanMe from "../ScanMe";
 import Link from "next/link";
 
 const socialLinks = [
-  { href: "https://github.com/mnamesujit", icon: <ImGithub className="text-3xl" /> },
+  { href: "https://github.com/mnamesujit", icon: <FaGithubAlt className="text-[1.7rem]" /> },
+  { href: "https://github.com/mnamesujit", icon: <VscTerminalBash className="text-[1.7rem]" /> },
   {
     href: "https://www.linkedin.com/in/mnamesujit",
-    icon: <FaLinkedinIn className="text-3xl" />,
+    icon: <FaLinkedinIn className="text-[1.7rem]" />,
   },
-  {
-    href: "https://wa.me/919305083852?text=Hey%21%20%F0%9F%91%8B%20Sujit",
-    icon: <RiWhatsappFill className="text-[32px]" />,
-  },
-  {
-    href: "#",
-    icon: <PiInstagramLogoFill className="text-3xl" />,
-  },
-  { href: "https://twitter.com/mnamesujit", icon: <GrTwitter className="text-3xl" /> },
-  { href: "/sujit-kumar.pdf", icon: <FaFilePdf className="text-[1.7rem]" download = "sujit-kumar.pdf" /> },
+  { href: "https://twitter.com/mnamesujit", icon: <FaXTwitter className="text-[1.5rem]" /> },
+  { href: "/sujit-kumar.pdf", icon: <FaFilePdf className="text-[1.4rem]" download = "sujit-kumar.pdf" /> },
 ];
 
 const Header: React.FC = () => {
@@ -41,9 +34,9 @@ const Header: React.FC = () => {
   };
 
   return (
-    <div className="z-10 max-w-5xl w-full flex items-center justify-between font-mono px-0 pr-4 md:px-6 border-b border-gray-300 dark:border-neutral-800 md:border-none">
+    <div className="z-10 max-w-7xl w-full flex items-center justify-between font-mono px-0 pr-4 md:px-4 border-b border-gray-300 dark:border-neutral-800 md:border-none">
       <Link href={"/"}>
-        <p className="md:border-b border-gray-300 p-4 md:p-3  md:px-5 lg:py-2.5 md:backdrop-blur-2xl dark:border-neutral-800 md:w-auto md:rounded-full md:border">
+        <p className="md:border-b border-gray-300 p-4 md:p-3 md:px-4 lg:py-2.5 md:backdrop-blur-2xl dark:border-neutral-800 md:w-auto md:rounded-full md:border hover:hover:text-emerald-500">
           <code className="font-mono text-base md:text-sm font-bold">
             <span className="hidden md:inline">FullStack/StackShow</span>
             /Nucleus.dev
@@ -51,7 +44,7 @@ const Header: React.FC = () => {
         </p>
       </Link>
 
-      <div className="hidden lg:flex space-x-3 items-end justify-center lg:h-auto lg:w-auto">
+      <div className="hidden lg:flex space-x-3 items-end justify-center lg:h-auto lg:w-auto border border-neutral-800 rounded-full px-4 py-2">
             {socialLinks.map((link, index) => (
               <SocialLink key={index} href={link.href} icon={link.icon} />
             ))}
