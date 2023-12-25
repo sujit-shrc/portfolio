49d70a465d86c82ad4e5a089acbf41688f727b72
Restyled.io <commits@restyled.io>
2023-12-25 11:27:56 +0000

    Restyled by whitespace

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,7 +72,7 @@ export default function RootLayout({
             attribute="class"
             defaultTheme="dark"
             disableTransitionOnChange
-          > 
+          >
            <div className='min-h-screen min-w-screen flex flex-col items-center pt-0 md:pt-8'>
               <Header />
               {children}
