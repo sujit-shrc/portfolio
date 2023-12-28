00418597755f2359d899970d4ad4c5827f7f7746
Sujit <mnamesujit@gmail.com>
2023-12-28 08:50:04 +0000

    feat: hero page ui update

--- components/ScanMe/index.tsx
+++ components/ScanMe/index.tsx
@@ -1,13 +1,14 @@
 "use client"
 
 import { useState } from 'react';
-import { IoBarcodeOutline } from "react-icons/io5";
-import { RiCloseFill, RiShareForwardFill } from 'react-icons/ri';
+import { IoExitOutline } from "react-icons/io5";
+import { PiDevicesFill } from "react-icons/pi";
+import { FaShareFromSquare } from "react-icons/fa6";
 import QRCode from 'react-qr-code';
 
 const Index: React.FC = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const qrCodeURL = "https://nucleus-neon.vercel.app/";
+  const qrCodeURL = "https://www.nucleus-dev.me/";
 
   const openModal = () => {
     setIsModalOpen(true);
@@ -35,11 +36,15 @@ const Index: React.FC = () => {
   };
 
   return (
-    <div>
-      <IoBarcodeOutline
+    <div className='relative'>
+      <PiDevicesFill
         className="text-[32px] font-bold cursor-pointer hover:text-emerald-500"
         onClick={openModal}
       />
+      <span className="absolute inline-flex items-center justify-center top-1.5 left-0.5 flex h-2 w-2">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-sky-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-1 w-1 bg-sky-500"></span>
+      </span>
 
       {isModalOpen && (
         <div className="fixed inset-0 overflow-y-auto">
@@ -51,15 +56,15 @@ const Index: React.FC = () => {
             <div className="blur-container relative z-10">
               <div className="border border-slate-600 rounded-lg px-6 py-2">
                 <div className="flex justify-end mb-1">
-                  <button className="text-3xl" onClick={closeModal}>
-                    <RiCloseFill />
+                  <button className="text-3xl hover:text-red-500" onClick={closeModal}>
+                    <IoExitOutline />
                   </button>
                 </div>
                 <div className="flex flex-col justify-center">
                   <QRCode value={qrCodeURL} size={200} />
                   <div className='flex justify-between mt-2'>
-                    <p className="">Share My Profile</p>
-                    <RiShareForwardFill className="text-xl cursor-pointer" onClick={shareQRCode} />
+                    <p className="">Share My Profile!</p>
+                    <FaShareFromSquare className="text-xl cursor-pointer" onClick={shareQRCode} />
                   </div>
                 </div>
               </div>
