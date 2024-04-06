b5afdeb86c1ec9473dd2a7dcd6c4dcf1e7720996
Sujit <mnamesujit@gmail.com>
2024-04-06 21:31:48 +0530

    update name from pcname to realname

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,26 +1,44 @@
-"use client"
+"use client";
 
 import { useState } from "react";
 import SocialLink from "./SocialLinks";
 import ThemeSwitch from "../ThemeSwitcher";
 import ScanMe from "../ScanMe";
 import Link from "next/link";
-import { FaLinkedinIn, FaFilePdf, IoExit, FaGithubAlt, FaXTwitter, VscTerminalBash, CgMenuGridO } from '@lib/icons';
-
+import {
+  FaLinkedinIn,
+  FaFilePdf,
+  IoExit,
+  FaGithubAlt,
+  FaXTwitter,
+  VscTerminalBash,
+  CgMenuGridO,
+} from "@lib/icons";
 
 const socialLinks = [
-  { href: "https://github.com/mnamesujit", icon: <FaGithubAlt className="text-[1.7rem]" /> },
-  { href: "https://github.com/mnamesujit", icon: <VscTerminalBash className="text-[1.7rem]" /> },
+  {
+    href: "https://github.com/mnamesujit",
+    icon: <FaGithubAlt className="text-[1.7rem]" />,
+  },
+  {
+    href: "https://github.com/mnamesujit",
+    icon: <VscTerminalBash className="text-[1.7rem]" />,
+  },
   {
     href: "https://www.linkedin.com/in/mnamesujit",
     icon: <FaLinkedinIn className="text-[1.7rem]" />,
   },
-  { href: "/sujit-kumar.pdf", icon: <FaFilePdf className="text-[1.5rem]" download = "sujit-kumar.pdf" /> },
-  { href: "https://twitter.com/mnamesujit", icon: <FaXTwitter className="text-[1.5rem]" /> },
+  {
+    href: "/sujit-kumar.pdf",
+    icon: <FaFilePdf className="text-[1.5rem]" download="sujit-kumar.pdf" />,
+  },
+  {
+    href: "https://twitter.com/mnamesujit",
+    icon: <FaXTwitter className="text-[1.5rem]" />,
+  },
 ];
 
 const Header: React.FC = () => {
-
   const [showSocialLinks, setShowSocialLinks] = useState(false);
 
   const toggleSocialLinks = () => {
@@ -32,16 +50,19 @@ const Header: React.FC = () => {
       <Link href={"/"}>
         <p className="md:border-b border-gray-300 px-0 py-3 md:px-4 lg:py-2.5 md:backdrop-blur-2xl dark:border-neutral-800 md:w-auto md:rounded-full md:border hover:hover:text-emerald-500">
           <code className="font-mono text-base md:text-sm font-bold">
-            <span className="hidden md:inline">FullStack/StackShow</span>
-            /Nucleus.dev
+            <span className="hidden md:inline">
+              <span className="text-emerald-500">[</span>~/sujit@arch
+              <span className="text-emerald-500">]</span>
+            </span>
+            <span className="text-rose-600">[λ]</span>sujit.sh
           </code>
         </p>
       </Link>
 
       <div className="hidden lg:flex space-x-3 items-end justify-center lg:h-auto lg:w-auto border border-neutral-800 rounded-full px-4 py-2">
-            {socialLinks.map((link, index) => (
-              <SocialLink key={index} href={link.href} icon={link.icon} />
-            ))}
+        {socialLinks.map((link, index) => (
+          <SocialLink key={index} href={link.href} icon={link.icon} />
+        ))}
       </div>
 
       {/* Mobile View for Social links */}
@@ -58,8 +79,8 @@ const Header: React.FC = () => {
           onClick={toggleSocialLinks}
         />
       )}
-      
-       {showSocialLinks && (
+
+      {showSocialLinks && (
         <div className="sm:hidden fixed left-1 bottom-1 py-2 bg-gray-300 dark:bg-gray-950 dark:bg-opacity-90 rounded-full shadow-md">
           {socialLinks.map((link, index) => (
             <SocialLink key={index} href={link.href} icon={link.icon} />
@@ -70,7 +91,7 @@ const Header: React.FC = () => {
       <div className="flex items-center gap-4 md:gap-8">
         {/* Theme Switcher */}
         <ThemeSwitch />
-        <ScanMe />  
+        <ScanMe />
       </div>
     </div>
   );
